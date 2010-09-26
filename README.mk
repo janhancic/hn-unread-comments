d77084484845c71fdca31a32a624e6f7820fb237
Jan Hančič <jan.hancic@gmail.com>
2010-09-26 14:44:22 +0200

    UPD: added readme file

---
+++ README.mk
@@ -0,0 +1,8 @@
+# HN Unread Comments
+
+
+## What is it?
+A simple Google Chrome extension that display unread comments on [Hacker News](http://news.ycombinator.com "Hacker News").
+
+## How to use it?
+Download and install the extension from the [Google Chrome extensions page](https://chrome.google.com/extensions/ "Chrome Extensions") (link to follow).
