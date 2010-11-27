bd75d45351fab14bcadd4bd875710dad46514302
Jan Hančič <jan.hancic@gmail.com>
2010-11-27 14:34:29 +0100

    UPD: improve comments 'detection', bump version number to 1.1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//localStorage.clear ();
+//localStorage.clear (); // for testing purposes
+
+// remove old read comments storage
+localStorage.removeItem ( 'read_comments' );
+
 Storage.prototype.setObject = function ( key, value ) {
 	this.setItem ( key, JSON.stringify ( value ) );
 };
@@ -16,11 +20,19 @@ Storage.prototype.getObject = function ( key ) {
 // get the background colour of the top navigation bar
 var unreadColour = $( 'table:first td:first' ).attr ( 'bgcolor' );
 
-// get all comments (anchors) on this page
-var $comments = $( "a[id^='up']:gt(0)" );
+// get all comments on this page
+var $comments = null;
+
+if ( ( document.location + '' ).indexOf ( 'item?id=' ) > 0 ) {
+	// we are on a item page
+	$comments = $( 'body > center > table > tbody > tr:eq(2) > td table:eq(1) > tbody > tr' );
+} else {
+	// we are on threads page
+	$comments = $( 'body > center > table > tbody > tr:gt(2)' );
+}
 
 // get all stored unread comments
-var readComments = localStorage.getObject ( 'read_comments' );
+var readComments = localStorage.getObject ( 'read_comments_list' );
 if ( readComments === undefined || readComments === null ) {
 	readComments = {};
 }
@@ -28,12 +40,14 @@ if ( readComments === undefined || readComments === null ) {
 $comments.each ( function () {
 	var $this = $( this );
 
-	if ( readComments[$this.attr ( 'id' )] === undefined ) {
-		// mark this comments as unread
-		$this.parents ( 'table:first' ).find ( 'td:eq(2)' ).css ( 'border', '1px solid ' + unreadColour );
+	var commentContentMd5 = md5 ( $this.find ( 'table td:eq(2) span.comment' ).text () );
 
-		readComments[$this.attr ( 'id' )] = true;
+	if ( readComments[commentContentMd5] === undefined ) {
+		// mark this comments as unread
+		$this.find ( 'table td:eq(2)' ).css ( 'border', '1px solid ' + unreadColour );
+
+		readComments[commentContentMd5] = true;
 	}
 } );
 
-localStorage.setObject ( 'read_comments', readComments );
+localStorage.setObject ( 'read_comments_list', readComments );
