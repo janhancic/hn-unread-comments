48e367e6f7b0d1880812f121b2ac8207958021b5
Jan Hančič <jan.hancic@gmail.com>
2013-10-12 17:40:35 +0100

    Update JS formating

--- src/hn_unread_comments.js
+++ src/hn_unread_comments.js
@@ -1,24 +1,24 @@
 //localStorage.clear (); // for testing purposes
 
 // remove old read comments storage
-localStorage.removeItem ( 'read_comments' );
+localStorage.removeItem( 'read_comments' );
 
-Storage.prototype.setObject = function ( key, value ) {
-	this.setItem ( key, JSON.stringify ( value ) );
+Storage.prototype.setObject = function( key, value ) {
+	this.setItem( key, JSON.stringify( value ) );
 };
 
 Storage.prototype.getObject = function ( key ) {
-	var item = this.getItem ( key );
+	var item = this.getItem( key );
 
 	if ( item === undefined ) {
 		return undefined;
 	}
 
-	return JSON.parse ( item );
+	return JSON.parse( item );
 };
 
 // get the background colour of the top navigation bar
-var unreadColour = $( 'table:first td:first' ).attr ( 'bgcolor' );
+var unreadColour = $( 'table:first td:first' ).attr( 'bgcolor' );
 
 // get all comments on this page
 var $comments = null;
@@ -32,22 +32,22 @@ if ( ( document.location + '' ).indexOf ( 'item?id=' ) > 0 ) {
 }
 
 // get all stored unread comments
-var readComments = localStorage.getObject ( 'read_comments_list' );
+var readComments = localStorage.getObject( 'read_comments_list' );
 if ( readComments === undefined || readComments === null ) {
 	readComments = {};
 }
 
-$comments.each ( function () {
+$comments.each( function () {
 	var $this = $( this );
 
-	var commentContentMd5 = md5 ( $this.find ( 'table td:eq(2) span.comment' ).text () );
+	var commentContentMd5 = md5( $this.find( 'table td:eq(2) span.comment' ).text() );
 
 	if ( readComments[commentContentMd5] === undefined ) {
 		// mark this comments as unread
-		$this.find ( 'table td:eq(2)' ).css ( 'border', '1px solid ' + unreadColour );
+		$this.find( 'table td:eq(2)' ).css( 'border', '1px solid ' + unreadColour );
 
 		readComments[commentContentMd5] = true;
 	}
 } );
 
-localStorage.setObject ( 'read_comments_list', readComments );
+localStorage.setObject( 'read_comments_list', readComments );
